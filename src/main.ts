611c7239b07dcabcf3e315d69b00185398cbc3aa
clarisse <clarissebleasby@gmail.com>
2026-03-02 09:32:24 -0300

    update version

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  //hora
+  process.env.TZ = '-03:00';
+
+  //validar biblioteca
+  app.useGlobalPipes(new ValidationPipe());
+  //para aceitar requisições de outros domínios
+  app.enableCors();
+
+  await app.listen(process.env.PORT ?? 4000);
 }
 bootstrap();
