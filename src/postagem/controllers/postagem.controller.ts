4beba221e711b53c956001223a9244bb9a0a2451
clarisse <clarissebleasby@gmail.com>
2026-03-03 15:03:10 -0300

    ✨ New Feature: Construcao do recurso postagem

--- src/postagem/controllers/postagem.controller.ts
+++ src/postagem/controllers/postagem.controller.ts
@@ -1,6 +1,7 @@
-import { Controller, Get } from "@nestjs/common";
+import { Body, Controller, Delete, Get, HttpCode, HttpStatus, Param, ParseIntPipe, Post, Put } from "@nestjs/common";
 import { PostagemService } from "../services/postagem.service";
 import { Postagem } from "../entities/postagem.entities";
+import { DeleteResult } from "typeorm";
 
 
 @Controller("/postagens") //decorador para indicar que essa classe é um controller e vai responder pelas rotas /postagens
@@ -13,7 +14,50 @@ export class PostagemController {
 
     //criar o metodo Get para listar todas as postagens - findAll() - retorna uma Promise de um array de Postagem
     @Get() //decorador para indicar que esse método vai responder a requisições GET na rota /postagens
-    async findAll(): Promise<Postagem[]> {
-        return await this.postagemService.findAll(); 
+    @HttpCode(HttpStatus.OK)
+    findAll(): Promise<Postagem[]> {
+        return this.postagemService.findAll(); 
     }
+
+    //busca por id
+    @Get('/:id') //decorador para indicar que esse método vai responder a requisições GET na rota /postagens : indica que o id é uma variavel de caminho
+    @HttpCode(HttpStatus.OK) //decorador para indicar que esse método vai responder com o status 200 OK como padrao de resposta
+    findById(@Param('id', ParseIntPipe) id: number): Promise<Postagem> { //o valor q vem no GET() chega como string, entao o ParseIntPipe converte para number
+        return this.postagemService.findById(id); 
+    }
+
+    //busca por titulo
+    @Get('/titulo/:titulo') //-> /: indica que o titulo é uma variavel de caminho --- /titulo/ é como uma etiqueta do tipo de busca que estamos fazendo, para diferenciar de outras buscas por atributo que possam existir
+    @HttpCode(HttpStatus.OK) 
+    findAllByTitulo(@Param('titulo') titulo: string): Promise<Postagem[]> { 
+        return this.postagemService.findAllByTitulo(titulo); 
+    }
+
+    //criar uma nova postagem - save() - recebe um objeto do tipo Postagem e retorna o objeto salvo
+    
+    //@Post() - recebe o objeto do tipo Postagem, usamos o decorador 
+    //@Body() - indica que o objeto do tipo Postagem vai chegar no corpo da requisição - entao o cliente vai enviar um JSON com os dados da postagem no corpo da requisição
+    //verbo diferente pode manter o msm caminho - nesse caso /postagens
+    @Post()
+    @HttpCode(HttpStatus.CREATED) // create é o status de resposta para quando um recurso é criado com sucesso - 201
+    create(@Body() postagem: Postagem): Promise<Postagem> {
+        return this.postagemService.create(postagem); 
+    }
+
+    // atualizar uma postagem - update() 
+    // recebe um objeto do tipo Postagem e retorna o objeto atualizado
+    @Put()
+    @HttpCode(HttpStatus.OK) 
+    update(@Body() postagem: Postagem): Promise<Postagem> {
+        return this.postagemService.update(postagem); 
+    }
+
+    //metodo para deletar uma postagem - delete() 
+    //para deletar, o verbo é DELETE e o caminho tem que receber o id da postagem para saber qual postagem deletar - entao usamos /:id
+    @Delete('/:id')
+    @HttpCode(HttpStatus.NO_CONTENT) //204 é o status de resposta para quando um recurso é deletado com sucesso - no content indica que a resposta nao tem conteudo, ou seja, nao retorna nada
+    delete(@Param('id', ParseIntPipe) id: number){
+        return this.postagemService.delete(id); 
+    }
+
 }
--- src/postagem/services/postagem.service.ts
+++ src/postagem/services/postagem.service.ts
@@ -1,6 +1,6 @@
-import { Injectable } from "@nestjs/common";
+import { HttpException, HttpStatus, Injectable } from "@nestjs/common";
 import { InjectRepository } from "@nestjs/typeorm";
-import { Repository } from "typeorm";
+import { DeleteResult, ILike, Repository } from "typeorm";
 import { Postagem } from "../entities/postagem.entities";
 
 //repository é a camada de acesso a dados, onde ficam os métodos para acessar o banco de dados (find, save, delete, etc)
@@ -16,7 +16,49 @@ export class PostagemService {
     //metodo para listar todas as postagens - findAll() - retorna uma Promise de um array de Postagem
     async findAll(): Promise<Postagem[]> {
         return await this.postagemRepository.find(); //select * from tb_postagens
+    }  
+    
+    //metodo para listar uma postagem por id - findOne() - retorna um objeto
+    async findById(id: number): Promise<Postagem> {
+        //Guardar a resposta numa const para saber se achou o resultado ou nao
+        const postagem = await this.postagemRepository.findOne({
+            where: { id }
+    }) //select * from tb_postagens where id = ?
+      if (!postagem) 
+        throw new HttpException(`Postagem com id ${id} não encontrada`, HttpStatus.NOT_FOUND); //lançar um erro caso a postagem não seja encontrada
+      return postagem;  //se nao tiver erro, retorna a postagem encontrada
     }
 
-    
+    //metodo de busca por atributo especifico - findAllByTitulo() - retorna um array de Postagem
+    async findAllByTitulo(titulo: string): Promise<Postagem[]> {
+        return this.postagemRepository.find({
+            where: {
+                titulo: ILike(`%${titulo}%`) //select * from tb_postagens where titulo like '%titulo%'
+            }
+        })
+    }
+
+    //metodo para criar uma nova postagem - save() - recebe um objeto do tipo Postagem e retorna o objeto salvo
+    async create(postagem: Postagem): Promise<Postagem> {
+        return await this.postagemRepository.save(postagem); //insert into tb_postagens (titulo, texto) values (?, ?)
+    }
+
+    //metodo para atualizar uma postagem - update() - recebe um objeto do tipo Postagem e retorna o objeto atualizado
+    async update(postagem: Postagem): Promise<Postagem> {
+        //update tb_postagens set titulo = ?, texto = ?, data = current_timestamp() where id = ?
+        //preciso saber se o id da postagem existe para atualizar, entao chamo o findById() - se nao encontrar, ele lança um erro
+        if(!postagem.id || postagem.id <= 0) 
+            throw new HttpException("ID da postagem inválido", HttpStatus.BAD_REQUEST);
+        
+        await this.findById(postagem.id);
+        return this.postagemRepository.save(postagem); //save() - se o id existir, ele atualiza, se nao existir, ele cria
+    }
+
+    //metodo para deletar uma postagem - delete() - recebe o id da postagem
+    async delete(id: number): Promise<DeleteResult> {         
+        await this.findById(id);        //chamar o findById() para verificar se a postagem existe, se nao existir, ele lança um erro
+        
+        //delete from tb_postagens where id = ?
+        return this.postagemRepository.delete(id);   //se existir, ele deleta a postagem e retorna o resultado da operação
+    }
 }
