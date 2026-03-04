2275131521baf6455ca2b56db8b04b5352f93775
clarisse <clarissebleasby@gmail.com>
2026-03-04 12:19:23 -0300

    ✨ New Feature: Construção do CRUD do recurso Tema

--- src/tema/controllers/tema.controller.ts
+++ src/tema/controllers/tema.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, HttpCode, HttpStatus } from "@nestjs/common";
+import { Body, Controller, Delete, Get, HttpCode, HttpStatus, Param, ParseIntPipe, Post, Put } from "@nestjs/common";
 import { TemaService } from "../services/tema.service";
 import { Tema } from "../entities/tema.entities";
 
@@ -11,13 +11,46 @@ export class TemaController {
         private readonly temaService: TemaService
     ){}
 
-    //criar o metodo Get para listar todos os temas - findAll() - retorna uma Promise de um array de temas
+    //Get - listar todos os temas - findAll() - retorna uma Promise de um array de temas
         @Get() //decorador para indicar que esse método vai responder a requisições GET na rota /temas
         @HttpCode(HttpStatus.OK)
         findAll(): Promise<Tema[]> {
             return this.temaService.findAll(); 
         }
 
-    
+    //Get - busca um tema por id
+    @Get('/:id') //decorador para indicar que esse método vai responder a requisições GET na rota /temas : indica que o id é uma variavel de caminho
+    @HttpCode(HttpStatus.OK) //decorador para indicar que esse método vai responder com o status 200 OK como padrao de resposta
+    findById(@Param('id', ParseIntPipe) id: number): Promise<Tema> { //o valor q vem no GET() chega como string, entao o ParseIntPipe converte para number
+        return this.temaService.findById(id); 
+    }
+
+    //Get - busca um tema por descricao
+    @Get('/descricao/:descricao') //decorador para indicar que esse método vai responder a requisições GET na rota /temas : indica que a descricao é uma variavel de caminho
+    @HttpCode(HttpStatus.OK) //decorador para indicar que esse método vai responder com o status 200 OK como padrao de resposta
+    findAllByDescricao(@Param('descricao') descricao: string): Promise<Tema[]> { 
+        return this.temaService.findAllByDescricao(descricao); 
+    }
+
+    //Post - criar um novo tema - create() - recebe um objeto do tipo Tema e retorna o objeto salvo
+    @Post()
+    @HttpCode(HttpStatus.CREATED) // create é o status de resposta para quando um recurso é criado com sucesso - 201
+    create(@Body() tema: Tema): Promise<Tema> {
+        return this.temaService.create(tema); 
+    }
+
+    //@put - atualizar um tema - update() - recebe um objeto do tipo Tema e retorna o objeto atualizado
+    @Put()
+    @HttpCode(HttpStatus.OK) 
+    update(@Body() tema: Tema): Promise<Tema> {
+        return this.temaService.update(tema); 
+    }
+
+    //@delete - deletar um tema - delete() - recebe o id do tema para deletar
+    @Delete('/:id')
+    @HttpCode(HttpStatus.NO_CONTENT) //204 é o status de resposta para quando um recurso é deletado com sucesso - no content indica que a resposta nao tem conteudo, ou seja, nao retorna nada
+    delete(@Param('id', ParseIntPipe) id: number){
+        return this.temaService.delete(id); 
+    }  
 
 }
--- src/tema/services/tema.service.ts
+++ src/tema/services/tema.service.ts
@@ -1,7 +1,7 @@
-import { Injectable } from "@nestjs/common";
+import { HttpException, HttpStatus, Injectable } from "@nestjs/common";
 import { InjectRepository } from "@nestjs/typeorm";
 import { Tema } from "../entities/tema.entities";
-import { Repository } from "typeorm";
+import { ILike, Repository, DeleteResult } from "typeorm";
 
 @Injectable() 
 export class TemaService {
@@ -16,5 +16,45 @@ export class TemaService {
     async findAll(): Promise<Tema[]> {
         return await this.temaRepository.find(); //select * from tb_temas
     }  
-            
+
+    //metodo para listar um tema por id - findById() -
+    //o retorno do FindById é um obj e nao uma array, por isso necessario criar uma constante para guardar a resposta e verificar se existe ou nao o tema
+    async findById(id: number): Promise<Tema> {
+       const tema = await this.temaRepository.findOne({
+            where: { id }
+        })         //select * from tb_temas where id = ? 
+        if (!tema) 
+            throw new HttpException(`Tema com id ${id} não encontrado`, HttpStatus.NOT_FOUND); //lançar um erro caso o tema nao seja encontrado
+        return tema; //se nao tiver erro, retorna o tema encontrado
+    }
+
+    //metodo para buscar por descricao - findAllByDescricao() - retorna um array de Tema
+    async findAllByDescricao(descricao: string): Promise<Tema[]> {
+        return this.temaRepository.find({
+            where: {
+                descricao: ILike(`%${descricao}%`) //select * from tb_temas where descricao like '%descricao%'
+            }
+        })
+    }
+    // metodo para criar um novo tema - save() - recebe um objeto do tipo Tema e retorna o objeto salvo
+    async create(tema: Tema): Promise<Tema> {
+        return await this.temaRepository.save(tema); //insert into tb_temas (descricao) values (?)
+    }
+
+    //metodo para atualizar um tema - update() - recebe um objeto do tipo Tema e retorna o objeto atualizado
+    async update(tema: Tema): Promise<Tema> {
+        //update tb_temas set descricao = ? where id = ?
+        //preciso saber se o id do tema existe para atualizar, entao chamo o findById() - se nao encontrar, ele lança um erro
+        if(!tema.id || tema.id <= 0) 
+            throw new HttpException("ID do tema inválido", HttpStatus.BAD_REQUEST);
+        
+        await this.findById(tema.id);
+        return this.temaRepository.save(tema);
+    }
+
+    //metodo para deletar um tema - delete() - recebe o id do tema
+    async delete(id: number): Promise<DeleteResult> {         
+        await this.findById(id);        //chamar o findById() para verificar se o tema existe, se nao existir, ele lança um erro
+        return this.temaRepository.delete(id); //delete from tb_temas where id = ?
+    }
 }
